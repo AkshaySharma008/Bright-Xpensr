6ee5f5d237c323cd72a54a3a61590ddd94f5037b
AkshaySharma008 <akshaysharma30121999@gmail.com>
2021-08-27 20:44:32 +0530

    Time Searies Chart v1

--- src/components/expense-analysis/index.js
+++ src/components/expense-analysis/index.js
@@ -1,63 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./expense-analysis.css";
-import { Bar } from "react-chartjs-2";
 import { useSelector } from "react-redux";
-
-const arbitraryStackKey = "stack1";
+import { Line } from "react-chartjs-2";
+import Dropdown from "../all-expenses/dropdown";
+import { MonthsArray, MonthsMapWithNumber } from "../constants/expenses";
+import dateFormat from "dateformat";
 
 const data = {
-  labels: [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "Spetember",
-    "October",
-    "November",
-    "December",
-  ],
+  labels: [],
   datasets: [
     {
-      label: "Category-1",
-      data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
+      label: "Expense Amount (In INR)",
+      data: [],
+      fill: false,
       backgroundColor: "rgb(255, 99, 132)",
-      stack: arbitraryStackKey,
-    },
-    {
-      label: "Category-2",
-      data: [2, 3, 20, 5, 1, 4, 2, 3, 20, 5, 1, 4],
-      backgroundColor: "rgb(54, 162, 235)",
-      stack: arbitraryStackKey,
-    },
-    {
-      label: "Category-3",
-      data: [3, 10, 13, 15, 22, 30, 13, 15, 22, 30, 13, 15],
-      backgroundColor: "rgb(75, 192, 192)",
-      stack: arbitraryStackKey,
-    },
-    {
-      label: "Category-4",
-      data: [3, 10, 13, 15, 22, 30, 13, 15, 22, 30, 13, 15],
-      backgroundColor: "rgb(70, 112, 192)",
-      stack: arbitraryStackKey,
-    },
-    {
-      label: "Category-5",
-      data: [3, 10, 13, 15, 22, 30, 13, 15, 22, 30, 13, 15],
-      backgroundColor: "rgb(75, 180, 192)",
-      stack: arbitraryStackKey,
+      borderColor: "rgba(255, 99, 132, 0.2)",
     },
   ],
 };
+
 const options = {
   scales: {
     yAxes: [
       {
-        stacked: true,
         ticks: {
           beginAtZero: true,
         },
@@ -65,28 +30,57 @@ const options = {
     ],
     xAxes: [
       {
-        stacked: true,
+        type: "time",
+        distribution: "series",
       },
     ],
   },
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Monthly Expenses (In INR) Stacked Bar Graph",
-      position: "bottom",
-    },
-  },
+};
+
+const dropdownMonths = [{ types: [...MonthsArray] }];
+
+const getAllDatesInAMonth = (selectedMonth, list) => {
+  // Filter all data of given month and add them into all dates and amount into an array
+  const monthRank = MonthsMapWithNumber[selectedMonth];
+  let dateArrayInMonth = [];
+  let amountArrayInMonth = [];
+  const expenseDatesInMonthArray = list.filter(
+    (data) => new Date(data.dueDate).getMonth() + 1 === monthRank
+  );
+  for (let data of expenseDatesInMonthArray) {
+    amountArrayInMonth.push(parseInt(data.amount));
+    dateArrayInMonth.push(dateFormat(data.dueDate, "dd-mm-yyyy"));
+  }
+  return { expenseDatesInMonthArray, dateArrayInMonth, amountArrayInMonth };
 };
 
 const ExpenseAnalysis = () => {
   let { expenseList: list } = useSelector((state) => state.expenses);
-  console.log(list);
+  const [currentSelectedMonth, setCurrentSelectedMonth] = useState("January");
+  const filterByMonth = (month) => {
+    setCurrentSelectedMonth(month);
+  };
+  const { expenseDatesInMonthArray, dateArrayInMonth, amountArrayInMonth } =
+    getAllDatesInAMonth(currentSelectedMonth, list);
+  console.log(expenseDatesInMonthArray, dateArrayInMonth, amountArrayInMonth);
+  // this.chartData.label = dateArrayInMonth;
+  // this.chartData.datasets[0].data = amountArrayInMonth;
+  // this.chartData.update();
+
   return (
-    <div className="chart">
-      <Bar data={data} options={options} />
+    <div className="analytics-chart">
+      <div>
+        <label className="dropdown-title-label">Select Month</label>
+        <Dropdown
+          className="month-dropdown"
+          options={dropdownMonths}
+          onSelect={filterByMonth}
+          toShow={currentSelectedMonth}
+        />
+      </div>
+      <div className="chart">
+        <Line id="expenseChart" data={data} options={options} />
+      </div>
     </div>
   );
 };
